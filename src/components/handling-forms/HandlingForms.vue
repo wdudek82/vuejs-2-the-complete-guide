5833b5adda55f51460f1e5fb1de9ab660035cd91
Wojciech Dudek <wdudek82@gmail.com>
2019-03-21 16:40:47 +0100

    Constrolling forms in Vue

--- src/components/handling-forms/HandlingForms.vue
+++ src/components/handling-forms/HandlingForms.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <form action="">
+    <form v-if="!isSubmitted">
       <div class="row">
         <div
             class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
@@ -9,15 +9,30 @@
           <hr />
           <div class="form-group">
             <label for="email">Mail</label>
-            <input id="email" v-model="userData.email" type="text" class="form-control" />
+            <input
+                id="email"
+                v-model="userData.email"
+                type="text"
+                class="form-control"
+            />
           </div>
           <div class="form-group">
             <label for="password">Password</label>
-            <input id="password" v-model="userData.password" type="password" class="form-control" />
+            <input
+                id="password"
+                v-model="userData.password"
+                type="password"
+                class="form-control"
+            />
           </div>
           <div class="form-group">
             <label for="age">Age</label>
-            <input id="age" v-model="userData.age" type="number" class="form-control" />
+            <input
+                id="age"
+                v-model="userData.age"
+                type="number"
+                class="form-control"
+            />
           </div>
         </div>
       </div>
@@ -26,21 +41,40 @@
             class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
         >
           <label for="message">Message</label><br />
-          <textarea id="message" v-model="message" rows="5" class="form-control"></textarea>
+          <textarea
+              id="message"
+              v-model="message"
+              rows="5"
+              class="form-control"
+              placeholder="Message text..."
+          ></textarea>
         </div>
       </div>
       <div class="row">
         <div
             class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >
-          <label for="sendmail">
-            <input id="sendmail" type="checkbox" value="SendMail" /> Send Mail
-          </label>
-          <label for="sendInfomail">
-            <input id="sendInfomail" type="checkbox" value="SendInfomail" />
-            Send Infomail
-          </label>
-          <!-- Also show for single checkbox with true/false -->
+          <div class="form-group">
+            <label for="sendmail">
+              <input
+                  id="sendmail"
+                  v-model="sendMail"
+                  type="checkbox"
+                  value="SendMail"
+              />
+              Send Mail
+            </label>
+            <label for="sendInfomail">
+              <input
+                  id="sendInfomail"
+                  v-model="sendMail"
+                  type="checkbox"
+                  value="SendInfomail"
+              />
+              Send Infomail
+            </label>
+            <!-- Also show for single checkbox with true/false -->
+          </div>
         </div>
       </div>
       <div class="row">
@@ -48,10 +82,12 @@
             class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
         >
           <label for="male">
-            <input id="male" type="radio" value="male" /> Male
+            <input id="male" v-model="gender" type="radio" value="male" />
+            Male
           </label>
           <label for="female">
-            <input id="female" type="radio" value="female" /> Female
+            <input id="female" v-model="gender" type="radio" value="female" />
+            Female
           </label>
         </div>
       </div>
@@ -61,9 +97,23 @@
             class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
         >
           <label for="priority">Priority</label><br />
-          <select id="priority" class="form-control">
-            <option value=""></option>
+          <select id="priority" v-model="selectedPriority" class="form-control">
+            <option v-for="priority in priorities" :key="priority">{{
+              priority
+              }}
+            </option>
           </select>
+        </div>
+      </div>
+      <div class="row">
+        <div
+            class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
+        >
+          <app-switch
+              v-model="dataSwitch"
+              :value="dataSwitch"
+              @input="dataSwitch = $event"
+          ></app-switch>
         </div>
       </div>
       <hr />
@@ -71,12 +121,12 @@
         <div
             class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!</button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div v-if="isSubmitted" class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <h4>Your Data</h4>
@@ -85,13 +135,14 @@
           <p>Mail: {{ userData.email }}</p>
           <p>Password: {{ userData.password }}</p>
           <p>Age: {{ userData.age }}</p>
-          <p>Message: {{ message }}</p>
+          <p style="white-space: pre;">Message: {{ message }}</p>
           <p><strong>Send Mail?</strong></p>
           <ul>
-            <li></li>
+            <li v-for="item in sendMail" :key="item">{{ item }}</li>
           </ul>
-          <p>Gender:</p>
-          <p>Priority:</p>
+          <p>Gender: {{ gender }}</p>
+          <p>Priority: {{ selectedPriority }}</p>
+          <p>Switched: {{ dataSwitch }}</p>
         </div>
       </div>
     </div>
@@ -99,8 +150,13 @@
 </template>
 
 <script>
+import AppSwitch from './form-components/AppSwitch';
+
 export default {
   name: 'HandlingForms',
+  components: {
+    AppSwitch,
+  },
   data() {
     return {
       userData: {
@@ -109,7 +165,18 @@ export default {
         age: 27,
       },
       message: '',
+      sendMail: [],
+      gender: 'male',
+      selectedPriority: 'Low',
+      priorities: ['High', 'Medium', 'Low'],
+      dataSwitch: true,
+      isSubmitted: false,
     };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = true;
+    },
   },
 };
 </script>
---
+++ src/components/handling-forms/form-components/AppSwitch.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div id="on" :class="{ active: value }" @click="switched(true)">On</div>
+    <div id="off" :class="{ active: !value }" @click="switched(false)">Off</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSwitch',
+  props: {
+    value: {
+      type: Boolean,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    switched(isOn) {
+      this.$emit('input', isOn);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#on,
+#off {
+  width: 40px;
+  height: 20px;
+  background: lightgray;
+  padding: 2px;
+  display: inline-block;
+  margin: 10px -2px;
+  box-sizing: content-box;
+  cursor: pointer;
+  text-align: center;
+}
+
+#on:hover,
+#on.active {
+  background: lightgreen;
+}
+
+#off:hover,
+#off.active {
+  background: lightcoral;
+}
+</style>
